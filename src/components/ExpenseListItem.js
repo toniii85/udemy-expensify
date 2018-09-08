15f8e5c21d532ad06b9a05d63005edabbfbdf89b
Toni Vicente García <antonio.vicente@sngular.team>
2018-09-08 20:18:52 +0200

    Setup format para fecha e importe

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+
+numeral.register("locale", "es", {
+  delimiters: {
+      thousands: " ",
+      decimal: ","
+  },
+  abbreviations: {
+      thousand: "k",
+      million: "m",
+      billion: "b",
+      trillion: "t"
+  },
+  ordinal : function (number) {
+      return number === 1 ? "er" : "o";
+  },
+  currency: {
+      symbol: "€"
+  }
+});
+numeral.locale("es");
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format("0,0[.]00 $")}
+      -
+      {moment(createdAt).format("DD [de] MMMM YYYY")}
+    </p>
   </div>
 );
 
